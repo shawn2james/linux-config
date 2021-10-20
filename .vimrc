194e85c951aa8f99783866b47535cb1d803c9717
Shawn James <shawn2james@gmail.com>
2021-10-20 18:36:46 +0530

    added a few patches to dmenu

--- .vimrc
+++ .vimrc
@@ -1,4 +1,5 @@
-:set tabstop=4
-:set shiftwidth=4
-:set relativenumber
+set tabstop=4
+set shiftwidth=4
+set relativenumber
+set ignorecase
 syntax on
--- dmenu-5.0/config.h
+++ dmenu-5.0/config.h
@@ -4,6 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;
 
+static unsigned int maxhist = 64;
+static int histnodup = 1;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Mononoki Nerd Font Mono:size=14"
@@ -11,7 +13,7 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#94a1b5", "#224582" },
+	[SchemeNorm] = { "#94a1b5", "#030312" },
 	[SchemeSel] = { "#a1bfc7", "#001a21" },
 	[SchemeNormHighlight] = {"#a1bfc7", "#001a21"},
 	[SchemeSelHighlight] = {"#a1bfc7", "#b73611"},
@@ -27,4 +29,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 2;
