15f641af5f2668751ef0e234cf4fa9826ef5d19f
Shawn James <shawn2james@gmail.com>
2021-10-26 19:21:53 +0530

    adding backups

---
+++ dmenu-5.0.backup.old/config.h
@@ -0,0 +1,30 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;
+
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Ubuntu:size=13"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#839496", "#001a21" },
+	[SchemeSel] = { "#839496", "#073642" },
+	[SchemeNormHighlight] = {"#a1bfc7", "#001a21"},
+	[SchemeSelHighlight] = {"#a1bfc7", "#b73611"},
+	[SchemeOut] = { "#3d7fb8", "#001a21" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 2;
